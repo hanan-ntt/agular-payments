d72421b56d37475c2b12855149f9fe033231e2d8
Hanan Ahmed <hanan.ahmed@nttdata.com>
2021-06-30 09:51:28 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pymnt';
+  name = '';
+  userdate = '';
+  useramount = 0;
+  userkm = 0;
+  onNameChange(value: string) {
+    this.name = value;
+  }
+  onDateChange(value: string) {
+    this.userdate = value;
+  }
+  onCurrencyChange(value: string) {
+    let parsedValue = parseFloat(value);
+    if (!isNaN(parsedValue)) {
+      this.useramount = parsedValue;
+    }
+  }
+  onMilesChange(value: string) {
+    let parsedValue = parseFloat(value);
+    if (!isNaN(parsedValue)) {
+      this.userkm = parsedValue;
+    }
+  }
 }
